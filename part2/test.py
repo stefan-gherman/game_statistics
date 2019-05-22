8102130de314d40018f7b2bb81b3db449240b947
Szilveszter Erdős <szilveszter.erdos@codecool.com>
2019-05-22 11:39:23 +0200

    remove unnecessary printing from part2 tests

--- part2/test.py
+++ part2/test.py
@@ -45,32 +45,22 @@ class Tester(unittest.TestCase):
     def test_1_get_most_played(self):
         result = reports.get_most_played(self.input_file)
         self.assertEqual(result, "Minecraft")
-        if result == "Minecraft":
-            print("Function 'get_most_played' is passed.")
 
     def test_2_sum_sold(self):
         result = reports.sum_sold(self.input_file)
         self.assertEqual(result, 187.16)
-        if result == 187.16:
-            print("Function 'sum_sold' is passed.")
 
     def test_3_get_selling_avg(self):
         result = reports.get_selling_avg(self.input_file)
         self.assertEqual(result, 7.798333333333333)
-        if result == 7.798333333333333:
-            print("Function 'get_selling_avg' is passed.")
 
     def test_4_count_longest_title(self):
         result = reports.count_longest_title(self.input_file)
         self.assertEqual(result, 52)
-        if result == 52:
-            print("Function 'count_longest_title' is passed.")
 
     def test_5_get_date_avg(self):
         result = reports.get_date_avg(self.input_file)
         self.assertEqual(result, 2003)
-        if result == 2003:
-            print("Function 'get_date_avg' is passed.")
 
     def test_6_get_game(self):
         result = reports.get_game(self.input_file, "Counter-Strike")
@@ -78,24 +68,16 @@ class Tester(unittest.TestCase):
                     "First-person shooter", "Valve Corporation"]
 
         self.assertEqual(result, expected)
-        if result == expected:
-            print("Function 'get_game' is passed.")
 
     def test_bonus_1_count_grouped_by_genre(self):
         result = reports.count_grouped_by_genre(self.input_file)
         expected_result = get_grouped_dic()
         self.assertEqual(result, expected_result)
 
-        if result == expected_result:
-            print("Bonus function 'count_grouped_by_genre' is passed.")
-
     def test_bonus_2_get_date_ordered(self):
         result = reports.get_date_ordered(self.input_file)
         expected_result = get_date_ordered()
         self.assertEqual(result, expected_result)
-
-        if result == expected_result:
-            print("Bonus function 'get_date_ordered' is passed.")
 
 
 def main():
