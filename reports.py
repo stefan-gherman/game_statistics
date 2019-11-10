0dac22a35d676ed8a37fd42931c7df0f19afea1c
Stefan Gherman <scgheman@gmail.com>
2019-11-10 22:38:15 +0200

    Add open file function for reports.py

--- reports.py
+++ reports.py
@@ -1,2 +1,21 @@
 
 # Report functions
+
+def open_file():
+
+    with open ('game_stat.txt') as game_text:
+
+        games_db = game_text.readlines()
+
+    
+
+    for line in range(len(games_db)):
+        games_db[line] = games_db[line].rstrip('\n').split('\t')
+
+    return games_db
+
+def count_games(text_file):
+    pass
+
+text_file = open_file()
+print(type(text_file))
